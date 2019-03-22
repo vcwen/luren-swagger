c8a17fd22f116b4d37179e9c8335ce58b7f757d5
Vincent Wen <wenwei1202@gmail.com>
2019-03-22 23:44:09 +0800

    feat(plugin): plugin swagger to luren

---
+++ src/swagger.ts
@@ -0,0 +1,177 @@
+import mount from 'koa-mount'
+import Router from 'koa-router'
+import serve from 'koa-static'
+import _ from 'lodash'
+import { CtrlMetadata, Luren, MetadataKey, RouteMetadata } from 'luren'
+import njk from 'nunjucks'
+import Path from 'path'
+import Url from 'url'
+import { getParams, getRequestBody, getResponses } from './utils'
+
+export interface IContact {
+  name: string
+  url: string
+  email: string
+}
+export interface ILicense {
+  name: string
+  url?: string
+}
+export interface IInfo {
+  title: string
+  version: string
+  description?: string
+  termsOfService?: string
+  contact?: IContact
+  license?: ILicense
+}
+
+export interface IServer {
+  url: string
+  description?: string
+  variables?: any
+}
+
+export interface IParameter {
+  name: string
+  in: string
+  schema?: any
+  description?: string
+  required?: boolean
+  deprecated?: boolean
+}
+
+export interface IMediaType {
+  schema: any
+  example?: any
+  examples?: any[]
+  encoding?: any
+}
+
+export interface IRequestBody {
+  description?: string
+  content: { [mediaType: string]: IMediaType }
+  required?: boolean
+}
+
+export interface IResponse {
+  description?: string
+  headers?: any
+  content: { [content: string]: IMediaType }
+}
+
+export interface IOperation {
+  tags?: string[]
+  summary?: string
+  description?: string
+  externalDocs?: any
+  operationId?: string
+  parameters?: IParameter[]
+  requestBody?: IRequestBody
+  responses: { [code: string]: IResponse }
+  callbacks?: any
+  deprecated?: boolean
+  security?: any[]
+  servers?: IServer[]
+}
+
+export interface IPath {
+  get?: IOperation
+  post?: IOperation
+  put?: IOperation
+  delete?: any
+  summary?: string
+  description?: string
+}
+
+export interface ITag {
+  name: string
+  description?: []
+}
+
+export interface IOpenApi {
+  openapi: string
+  info: IInfo
+  servers: IServer[]
+  paths: { [path: string]: IPath }
+  components?: any
+  security?: any[]
+  tags?: ITag[]
+  externalDocs?: any
+}
+
+export class Swagger {
+  private _info: IInfo
+  private _servers: IServer[]
+  private _openApi: any
+  private _path: string
+  constructor(options?: { info?: IInfo; servers?: IServer[]; path?: string }) {
+    this._info = (options && options.info) || { title: 'Luren Swagger', version: '1.0.0' }
+    this._servers = (options && options.servers) || [{ url: '/' }]
+    this._path = (options && options.path) || '/explorer'
+  }
+  public pluginify() {
+    return (luren: Luren) => {
+      const koa = luren.getKoa()
+      const openApi: IOpenApi = {
+        openapi: '3.0.0',
+        info: this._info,
+        servers: this._servers,
+        tags: [],
+        paths: {}
+      }
+      const router = new Router()
+      router.get('/explorer/swagger.json', async (ctx) => {
+        if (!this._openApi) {
+          const controllers = luren.getControllers()
+          for (const ctrl of controllers) {
+            const ctrlMetadata: CtrlMetadata = Reflect.getMetadata(MetadataKey.CONTROLLER, ctrl.constructor)
+            const tag: ITag = { name: ctrlMetadata.name }
+            if (openApi.tags) {
+              openApi.tags.push(tag)
+            } else {
+              openApi.tags = [tag]
+            }
+            const props = Object.getOwnPropertyNames(Object.getPrototypeOf(ctrl))
+            for (const prop of props) {
+              const routeMetadata: RouteMetadata = Reflect.getMetadata(MetadataKey.ROUTE, ctrl, prop)
+              if (!routeMetadata) {
+                continue
+              }
+              const pathObj: IPath = {}
+              pathObj[routeMetadata.method] = {
+                tags: [ctrlMetadata.name],
+                description: routeMetadata.desc,
+                responses: {},
+                deprecated: routeMetadata.deprecated
+              }
+              const operation: IOperation = pathObj[routeMetadata.method]
+              const params = getParams(ctrl, prop)
+              if (!_.isEmpty(params)) {
+                operation.parameters = params
+              }
+              const requestBody = getRequestBody(ctrl, prop)
+              if (!_.isEmpty(requestBody)) {
+                operation.requestBody = requestBody
+              }
+              const responses = getResponses(ctrl, prop)
+              operation.responses = responses
+              openApi.paths[routeMetadata.path] = openApi.paths[routeMetadata.path] || {}
+              openApi.paths[routeMetadata.path][routeMetadata.method.toLowerCase()] = operation
+            }
+          }
+          this._openApi = openApi
+        }
+        ctx.body = this._openApi
+      })
+      router.get(this._path, async (ctx) => {
+        ctx.body = njk.render(Path.resolve(__dirname, '../swagger-dist/index.html'), {
+          url: Url.resolve(ctx.href, Path.join(ctx.path, 'swagger.json')),
+          prefix: Path.join(ctx.path, 'assets')
+        })
+      })
+      koa.use(router.routes()).use(router.allowedMethods())
+      koa.use(mount(Path.join(this._path, 'assets'), serve(Path.resolve(__dirname, '../swagger-dist'))))
+    }
+  }
+}
---
+++ src/utils.ts
@@ -0,0 +1,98 @@
+import { List } from 'immutable'
+import toOpenApiSchema from 'json-schema-to-openapi-schema'
+import { MetadataKey, ParamMetadata, ResponseMetadata } from 'luren'
+import { IMediaType, IParameter, IRequestBody, IResponse } from './swagger'
+
+const isFileType = (schema: any) => {
+  return schema.type === 'string' && schema.format === 'binary'
+}
+
+export const getParams = (ctrl: object, propKey: string) => {
+  const paramsMetadata: List<ParamMetadata> = Reflect.getMetadata(MetadataKey.PARAM, ctrl, propKey)
+  if (!paramsMetadata) {
+    return []
+  }
+  let params: IParameter[] = []
+  for (const paramMetadata of paramsMetadata) {
+    if (paramMetadata.source === 'context' || paramMetadata.source === 'body') {
+      continue
+    }
+    if (paramMetadata.root) {
+      params = []
+      if (paramMetadata.schema.properties) {
+        const props = Object.getOwnPropertyNames(paramMetadata.schema.properties)
+        const requiredProps = paramMetadata.schema.required || []
+        for (const prop of props) {
+          const param: IParameter = {
+            name: paramMetadata.name,
+            in: paramMetadata.source,
+            required: requiredProps.includes(prop),
+            schema: toOpenApiSchema(paramMetadata.schema.properties[prop])
+          }
+          params.push(param)
+        }
+        return params
+      } else {
+        throw new TypeError("Parameter's type must be 'object' when it's root")
+      }
+    } else {
+      const param: IParameter = {
+        name: paramMetadata.name,
+        in: paramMetadata.source,
+        required: paramMetadata.required,
+        schema: paramMetadata.schema,
+        description: paramMetadata.desc
+      }
+      params.push(param)
+    }
+  }
+  return params
+}
+
+export const getRequestBody = (ctrl: object, prop: string) => {
+  const paramsMetadata: List<ParamMetadata> = Reflect.getMetadata(MetadataKey.PARAM, ctrl, prop)
+  if (!paramsMetadata) {
+    return {} as any
+  }
+  const body: IRequestBody = { content: {} }
+  let content = 'application/json'
+  let schema: any = { type: 'object', properties: {} }
+  for (const paramMetadata of paramsMetadata) {
+    if (paramMetadata.source === 'body') {
+      if (isFileType(paramMetadata.schema)) {
+        if (paramMetadata.root) {
+          content = paramMetadata.mime || 'application/octet-stream'
+        } else {
+          content = 'multipart/form-data'
+          schema.properties[paramMetadata.name] = paramMetadata.schema
+        }
+      } else {
+        if (paramMetadata.root) {
+          schema = paramMetadata.schema
+        } else {
+          schema.properties[paramMetadata.name] = paramMetadata.schema
+        }
+      }
+    }
+  }
+  body.content = { [content]: { schema: toOpenApiSchema(schema) } }
+  return body
+}
+
+export const getResponses = (ctrl: object, prop) => {
+  const responsesMetadata: Map<number, ResponseMetadata> = Reflect.getMetadata(MetadataKey.RESPONSE, ctrl, prop)
+  const responses: { [code: string]: IResponse } = {}
+  for (const [statusCode, resMetadata] of responsesMetadata) {
+    const response: IResponse = {} as any
+    const res: IMediaType = {} as any
+    let contentType = 'application/json'
+
+    if (isFileType(resMetadata.schema)) {
+      contentType = resMetadata.schema.mime || 'application/octet-stream'
+    }
+    res.schema = toOpenApiSchema(resMetadata.schema)
+    response.content = { [contentType]: res }
+    responses[statusCode] = response
+  }
+  return responses
+}
